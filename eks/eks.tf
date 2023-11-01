0d9abcf376e8a8c048a303d8256d6f24c72849f5
manujoshi0310 <manu.shandillya@gmail.com>
2023-11-01 15:15:15 +0530

    add eks files

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,12 +1,12 @@
 resource "aws_eks_cluster" "eks" {
     name = "ed-eks-01"
-    role_arn = aws_iam_role.master.anr
+    role_arn = aws_iam_role.master.arn
     vpc_config {
       subnet_ids = [aws_subnet.pub_sub1.id, aws_subnet.pub_sub2.id]
     }
     depends_on = [ 
         aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-        aws_iam_role_policy_attachment.AmazonEKSServerPolicy,
+        aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
         aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
         aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
         aws_subnet.pub_sub1,
